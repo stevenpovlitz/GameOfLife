5577c9eea1fe13625bff51b93ffe5fa32cb8a1f9
Steven <steven.povlitz@gmail.com>
2016-10-26 22:05:29 -0400

    Initial Commit

---
+++ GameOfLife/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameOfLife
+//
+//  Created by Steven Povlitz on 10/26/16.
+//  Copyright © 2016 Steven Povlitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
